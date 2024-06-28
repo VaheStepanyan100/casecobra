7c14987d050c9c0b1757095c99b7c26017d3cea2
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-06-28 15:54:42 +0400

    Add a feedback section to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Phone from '@/components/Phone';
+import { Icons } from '@/components/ui/Icons';
 import { Check, Star } from 'lucide-react';
 
 export default function Home() {
@@ -93,8 +94,23 @@ export default function Home() {
                 src="/line.png"
                 className="absolute w-20 -left-6 -bottom-6 select-none"
               />
-              <Phone className='w-64' imgSrc='/testimonials/1.jpg'/>
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{' '}
+              <span className="relative px-2">
+                customers{' '}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{' '}
+              say
+            </h2>
+            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
           </div>
         </MaxWidthWrapper>
       </section>
