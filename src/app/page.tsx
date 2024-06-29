d01fddf8926523b3e663665b0671f0753d4a7168
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-06-29 16:40:04 +0400

    Update page.tsxby adding feedbacks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,78 @@ export default function Home() {
               </span>{' '}
               say
             </h2>
-            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="fles flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and half months now and{' '}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading
+                  yellow-ish color after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Jonathan</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4, w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="fles flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually keep my phone together with my keys in my pocket and that let to some pretty heavy scratchmarks on all of my last phone cases. This one, besides a barely noticable scratch on the corner{' '}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new about half a year
+                  </span>
+                  . I dig it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Josh</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4, w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
