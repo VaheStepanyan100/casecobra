c4e5e7da9e1d8ecf940d14d8cb223571534dfa44
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-07-11 13:48:39 +0400

    Updata upload functionality

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,12 @@
-const Page = async () => {
-  return <p></p>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  return <p>{id}</p>;
 };
 
 export default Page;
