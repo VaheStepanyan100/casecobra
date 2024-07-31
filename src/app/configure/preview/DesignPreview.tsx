a8e60032b4df7d0e4060efe5a27fb0cd83ed109a
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-07-31 18:55:20 +0400

    Add details to DesignPreview page

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,18 +1,25 @@
-"use client"
+'use client';
 
 import Phone from '@/components/Phone';
 import { cn } from '@/lib/utils';
-import { COLORS } from '@/validators/option-validator';
+import { COLORS, MODELS } from '@/validators/option-validator';
 import { Configuration } from '@prisma/client';
+import { Check } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import Confetti from 'react-dom-confetti';
 
-const DesignPreview = ({configuration}: {configuration: Configuration}) => {
+const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const [showConfetti, setShowConfetti] = useState(false);
   useEffect(() => setShowConfetti(true));
 
-  const {color} = configuration;
-  const tw = COLORS.find((supportedColor) => supportedColor.value === color)?.tw
+  const { color, model } = configuration;
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === color
+  )?.tw;
+
+  const { label: modelLabel } = MODELS.options.find(
+    ({ value }) => value === model
+  )!;
 
   return (
     <>
@@ -20,11 +27,46 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
         aria-hidden="true"
         className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
       >
-        <Confetti active={showConfetti} config={{elementCount: 200, spread: 90}} />
+        <Confetti
+          active={showConfetti}
+          config={{ elementCount: 200, spread: 90 }}
+        />
       </div>
-      <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
-        <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
-          <Phone className={cn(`bg-${tw}`)} imgSrc={configuration.croppedImageUrl!} />
+      <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+        <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
+          <Phone
+            className={cn(`bg-${tw}`)}
+            imgSrc={configuration.croppedImageUrl!}
+          />
+        </div>
+        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Case
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-5 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
+              <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+                <li>Wireless charging compatible</li>
+                <li>TPU shock absorption</li>
+                <li>Packaging made from recycled materials</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className='font-medium text-zinc-950'>Materials</p>
+              <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+                <li>High-quality, durable material</li>
+                <li>Scratch and fingerprint resistant coating</li>
+              </ol>
+            </div>
+          </div>
         </div>
       </div>
     </>
@@ -32,4 +74,3 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
 };
 
 export default DesignPreview;
-
