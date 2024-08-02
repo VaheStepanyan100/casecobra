4f9542c9091b0de4809455d6385985c321292568
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-08-02 13:49:38 +0400

    Add Check out button

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import Phone from '@/components/Phone';
+import { Button } from '@/components/ui/button';
 import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products';
 import { cn, formatPrice } from '@/lib/utils';
 import { COLORS, FINISHES, MODELS } from '@/validators/option-validator';
 import { Configuration } from '@prisma/client';
-import { Check } from 'lucide-react';
+import { ArrowRight, Check } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import Confetti from 'react-dom-confetti';
 
@@ -105,6 +106,9 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
                   </p>
                 </div>
               </div>
+            </div>
+            <div className='mt-8 flex justify-end pb-12'>
+              <Button className='px-4 sm:px-6 lg:px-8'>Check out <ArrowRight className='h-4 w-4 ml-1.5 inline' /></Button>
             </div>
           </div>
         </div>
