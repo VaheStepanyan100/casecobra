71bf308abed89e9ea7b98ccbfd76faf2b5eea749
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-08-04 18:02:17 +0400

    Add payment system

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -6,12 +6,23 @@ import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products';
 import { cn, formatPrice } from '@/lib/utils';
 import { COLORS, FINISHES, MODELS } from '@/validators/option-validator';
 import { Configuration } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
 import { ArrowRight, Check } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import Confetti from 'react-dom-confetti';
+import { createCheckoutSession } from './actions';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/components/ui/use-toast';
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import LoginModal from '@/components/LoginModal';
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
-  const [showConfetti, setShowConfetti] = useState(false);
+  const router = useRouter();
+  const {toast} = useToast();
+  const { id } = configuration
+  const {user} = useKindeBrowserClient()
+  const [isLoadingModalOpen, setIsLoadingModalOpen] = useState<boolean>(false)
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
   useEffect(() => setShowConfetti(true));
 
   const { color, model, finish, material } = configuration;
@@ -24,8 +35,34 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   )!;
 
   let totalPrice = BASE_PRICE;
-  if (material === 'policarbonate') totalPrice += PRODUCT_PRICES.material.polycarbonate
-  if (finish === 'textured') totalPrice += PRODUCT_PRICES.finish.textured
+  if (material === 'policarbonate')
+    totalPrice += PRODUCT_PRICES.material.polycarbonate;
+  if (finish === 'textured') totalPrice += PRODUCT_PRICES.finish.textured;
+
+  const {mutate: createPaymentSession} = useMutation({
+    mutationKey: ['get-checkout-session'],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({ url }) => {
+      if (url) router.push(url);
+      else throw new Error('Unable to retrieve paymant URL.');
+    },
+    onError: () => {
+      toast({
+        title: 'Something went wrong',
+        description: 'There waw and error on our end. Please try again.',
+        variant: 'destructive',
+      });
+    },
+  });
+
+  const handleCheckout = () => {
+    if (user) {
+      createPaymentSession({configId: id})
+    } else {
+      localStorage.setItem("configurationId", id)
+      setIsLoadingModalOpen(true)
+    }
+  }
 
   return (
     <>
@@ -38,6 +75,9 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           config={{ elementCount: 200, spread: 90 }}
         />
       </div>
+
+      <LoginModal isOpen={isLoadingModalOpen} setIsOpen={setIsLoadingModalOpen} />
+
       <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
@@ -58,7 +98,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
             <div>
               <p className="font-medium text-zinc-950">Highlights</p>
-              <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
                 <li>Wireless charging compatible</li>
                 <li>TPU shock absorption</li>
                 <li>Packaging made from recycled materials</li>
@@ -66,49 +106,51 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
               </ol>
             </div>
             <div>
-              <p className='font-medium text-zinc-950'>Materials</p>
-              <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
                 <li>High-quality, durable material</li>
                 <li>Scratch and fingerprint resistant coating</li>
               </ol>
             </div>
           </div>
-          <div className='mt-8'>
-            <div className='bg-gray-50 p-6 sm:rounded-lg sm:p-8'>
-              <div className='flow-root text-sm'>
-                <div className='flex items-center justify-between py-1 mt-2'>
-                  <p className='text-gray-600'>Base price</p>
-                  <p className='font-medium text-gray-900'>
+          <div className="mt-8">
+            <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+              <div className="flow-root text-sm">
+                <div className="flex items-center justify-between py-1 mt-2">
+                  <p className="text-gray-600">Base price</p>
+                  <p className="font-medium text-gray-900">
                     {formatPrice(BASE_PRICE / 100)}
                   </p>
                 </div>
                 {finish === 'textured' ? (
-                  <div className='flex items-center justify-between py-1 mt-2'>
-                  <p className='text-gray-600'>Textured finish</p>
-                  <p className='font-medium text-gray-900'>
-                    {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
-                  </p>
-                </div>
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Textured finish</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
+                    </p>
+                  </div>
                 ) : null}
                 {material === 'policarbonate' ? (
-                  <div className='flex items-center justify-between py-1 mt-2'>
-                  <p className='text-gray-600'>Soft polycarbonate material</p>
-                  <p className='font-medium text-gray-900'>
-                    {formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}
-                  </p>
-                </div>
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Soft polycarbonate material</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}
+                    </p>
+                  </div>
                 ) : null}
-                <div className='my-2 h-px bg-gray-200' />
-                <div className='flex items-center justify-between py-2'>
-                  <p className='font-semibold text-gray-900'>Order total</p>
-                  <p className='font-semibold text-gray-900'>
+                <div className="my-2 h-px bg-gray-200" />
+                <div className="flex items-center justify-between py-2">
+                  <p className="font-semibold text-gray-900">Order total</p>
+                  <p className="font-semibold text-gray-900">
                     {formatPrice(totalPrice / 100)}
                   </p>
                 </div>
               </div>
             </div>
-            <div className='mt-8 flex justify-end pb-12'>
-              <Button className='px-4 sm:px-6 lg:px-8'>Check out <ArrowRight className='h-4 w-4 ml-1.5 inline' /></Button>
+            <div className="mt-8 flex justify-end pb-12">
+              <Button onClick={() => handleCheckout()} className="px-4 sm:px-6 lg:px-8">
+                Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
+              </Button>
             </div>
           </div>
         </div>
