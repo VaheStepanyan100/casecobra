cba544c7012b4e43bf6dbbf575fdd5c7528ebc86
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-08-09 18:44:32 +0400

    Add thank you page

---
+++ src/components/PhonePreview.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { CaseColor } from "@prisma/client";
+
+const PhonePreview = ({croppedImageUrl, color}: {
+  croppedImageUrl: string
+  color: CaseColor
+}) => {
+
+}
+
+export default PhonePreview;
